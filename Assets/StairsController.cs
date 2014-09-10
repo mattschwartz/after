a412b2597c16d8a42467202ee01fd9bc8b850b1c
Tyler Pixley <tylerpixley@gmail.com>
2014-09-10 09:14:06 -0500

    stairs in progress; can't rotate components

---
+++ Assets/StairsController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StairsController : MonoBehaviour {
+
+    public float Height;
+    public GameObject PlayerObject;
+    private bool FinishedClimbing = false;
+    private bool Entered = false;
+    public BoxCollider2D Top;
+    public BoxCollider2D StepCollision;
+
+    void Start()
+    {
+        Top.enabled = true;
+        StepCollision.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Entered = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Entered = false;
+        Top.enabled = true;
+        StepCollision.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Entered && Input.GetKeyDown(KeyCode.E))
+        {
+            Top.enabled = false;
+            StepCollision.enabled = true;
+        }
+    }
+}
