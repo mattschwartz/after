2ac893638b55bc2a3b678cd4a4b4923e4597b325
Tyler Pixley <tylerpixley@gmail.com>
2014-09-10 12:29:49 -0500

    stairs now functional; puzzle prepared

---
+++ Assets/StairColliderController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StairColliderController : MonoBehaviour {
+
+    public BoxCollider2D Steps;
+    public BoxCollider2D Top;
+
+	// Use this for initialization
+	void Start () {
+        Steps.enabled = false;
+        Top.enabled = true;
+	}
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Steps.enabled = false;
+        Top.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
