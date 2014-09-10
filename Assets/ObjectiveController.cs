6a8f3f49c47570584a1ea80651773b7c708db766
Matt Schwartz <schwamat@gmail.com>
2014-09-10 01:49:16 -0500

    Added quickest possible soulsplosion animation states.

--- Assets/ObjectiveController.cs
+++ Assets/ObjectiveController.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class ObjectiveController : MonoBehaviour {
 
     private bool Entered;
+    private Animator Animator;
 
 	// Use this for initialization
 	void Start () {
         Entered = false;
+        Animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Entered && Input.GetKeyDown(KeyCode.E)) {
-
+            Animator.SetTrigger("FreeSoul");
         }	
 	}
 
