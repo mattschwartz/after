6e506f63ac213e1a28a3379554af90173113f056
Matt Schwartz <schwamat@gmail.com>
2014-09-13 14:11:00 -0500

    Implementing basic controller code.

---
+++ UnityProject/Assets/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    #region Public Members
+
+    public Transform FollowTarget;
+    public SpriteRenderer SpriteBounds;
+
+    #endregion
+
+    #region Private Members
+
+    public float DampTime = 0.15f;
+    private Vector3 Velocity = Vector3.zero;
+    private float LeftBounds; 
+    private float RightBounds; 
+    private float BottomBounds;
+    private float TopBounds;
+
+    #endregion
+
+    void Start()
+    {
+        float vertExtent = Camera.main.camera.orthographicSize;
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+
+        LeftBounds = (float)(horzExtent - SpriteBounds.sprite.bounds.size.x / 2.0f);
+        RightBounds = (float)(SpriteBounds.sprite.bounds.size.x / 2.0f - horzExtent);
+        BottomBounds = (float)(vertExtent - SpriteBounds.sprite.bounds.size.y / 2.0f);
+        TopBounds = (float)(SpriteBounds.sprite.bounds.size.y / 2.0f - vertExtent);
+    }
+
+    void Update()
+    {
+        Vector3 point = camera.WorldToViewportPoint(FollowTarget.position);
+        Vector3 delta = FollowTarget.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+        Vector3 destination = transform.position + delta;
+
+        var pos = Vector3.SmoothDamp(transform.position, destination, ref Velocity, DampTime);
+        pos.x = Mathf.Clamp(pos.x, LeftBounds, RightBounds);
+        pos.y = Mathf.Clamp(pos.y, BottomBounds, TopBounds);
+
+        transform.position = pos;
+    }
+}
---
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Public members
+
+    public KeyCode InteractButton = KeyCode.E;
+    public KeyCode JumpButton = KeyCode.Space;
+    public Transform GroundCheck;
+    public LayerMask GroundLayerMask;
+    
+    #endregion
+
+    #region Private members
+
+    private bool Grounded = false;
+    private bool FacingRight = true;
+    private float Speed = 5f;
+    private float JumpForce = 700f;
+    private float GroundRadius = 0.2f;
+    private Animator Animator;
+
+    #endregion
+
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    #region Update
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void FixedUpdate()
+    {
+        IsGrounded();
+
+        float hMove = Input.GetAxis("Horizontal");
+        Animator.SetFloat("Velocity", Mathf.Abs(hMove));
+
+        rigidbody2D.velocity = new Vector2(hMove * Speed, rigidbody2D.velocity.y);
+
+        if (hMove > 0 && !FacingRight) {
+            Flip();
+        } else if (hMove < 0 && FacingRight) {
+            Flip();
+        }
+    }
+
+    #endregion
+
+    private void IsGrounded()
+    {
+        Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundRadius, GroundLayerMask);
+        // Animator.SetBool("Grounded", Grounded); // nyi
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(JumpButton)) {
+
+        }
+    }
+
+    // Cheap animations
+    private void Flip()
+    {
+        FacingRight = !FacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
