c07a4da8cde7a4c954e031f779fd89b2ebd0ca25
Matt Schwartz <schwamat@gmail.com>
2014-09-14 19:41:45 -0500

    Added typed text as directives.

---
+++ UnityProject/Assets/DirectiveController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectiveController : MonoBehaviour
+{
+
+    #region Public Members
+
+    public string DirectiveText;
+    public GUIText Label;
+
+    #endregion
+
+    #region Private Members
+
+    private bool DisplayGUI = false;
+    private int DrawnCharacters = 0;
+
+    #endregion
+
+    void FixedUpdate()
+    {
+        if (DisplayGUI) {
+            DrawnCharacters = Mathf.Min(DrawnCharacters + 1, DirectiveText.Length);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        DisplayGUI = true;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+            Label.text = DirectiveText.Substring(0, DrawnCharacters);
+        DisplayGUI = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Label.text = "";
+        DisplayGUI = false;
+        DrawnCharacters = 0;
+    }
+}
---
+++ UnityProject/Assets/TextDropShadow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextDropShadow : MonoBehaviour {
+
+    public GUIText ShadowText;
+    private GUIText Text;
+
+    void Start()
+    {
+        Text = GetComponent<GUIText>();
+        Text.pixelOffset = new Vector2(0, 2);
+        ShadowText.pixelOffset = new Vector2(2, 0);
+    }
+
+    void OnGUI()
+    {
+       ShadowText.text = Text.text;
+    }
+}
