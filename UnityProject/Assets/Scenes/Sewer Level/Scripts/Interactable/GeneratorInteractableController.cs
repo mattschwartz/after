f539d21e7dc67b9677b3993de089560940794e57
Matt Schwartz <schwamat@gmail.com>
2014-09-16 22:28:52 -0500

    Added lights out trigger.

---
+++ UnityProject/Assets/Scenes/Sewer Level/Scripts/Interactable/GeneratorInteractableController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratorInteractableController : InteractableController
+{
+    #region Public Members
+
+    public BoxCollider2D PowerOutageTrigger;
+
+    #endregion
+
+    public override void Interact()
+    {
+        PowerOutageTrigger.GetComponent<BoxCollider2D>().enabled = true;
+    }
+}
---
+++ UnityProject/Assets/Scenes/Sewer Level/Scripts/PowerOutageController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerOutageController : MonoBehaviour
+{
+    void Awake()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
