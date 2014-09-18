2b2581a6fddbe419d35c5969b0705f739a01e4e5
pixley <tylerpixley@gmail.com>
2014-09-18 17:06:03 -0500

    Added rope swing code - NOT YET FUNCTIONAL

---
+++ UnityProject/Assets/Scripts/RopeSwingController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Scene.SceneManagement;
+
+public class RopeSwingController : MonoBehaviour {
+
+	public GameObject PlayerObject;
+	public float Length;
+	public float MaxRotation;
+	private Transform Trans;
+    private bool Active;
+    private bool Entered;
+
+	// Use this for initialization
+	void Start () {
+		Active = false;
+
+		Trans = GetComponent<Transform>();
+	}
+
+	void OnTriggerEnter2D ()
+	{
+		Entered = true;
+	}
+
+	void OnTriggerExit2D ()
+	{
+		Entered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Entered && !Active)
+		{
+			Active = true;
+		}
+		else if (Entered && Active && Input.GetKeyDown(KeyCode.Space))
+		{
+			Active = false;
+		}
+		else if (Entered && Active)
+		{
+			//Physics shit goes here
+		}
+	}
+}
