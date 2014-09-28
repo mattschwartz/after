3079271d917d38238484f91ac16352e0a4c89b5a
Matt Schwartz <schwamat@gmail.com>
2014-09-28 12:40:29 -0500

    Main menu loads sewer level now

---
+++ UnityProject/Assets/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    private KeyCode ContinueButton = KeyCode.E;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(ContinueButton)) {
+            Application.LoadLevel("SewerLevel");
+        }
+    }
+}
