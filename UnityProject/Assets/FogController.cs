4ffb07321c15691af5e6ff263fa363cc22444522
Matt Schwartz <schwamat@gmail.com>
2014-09-30 21:10:28 -0500

    Fixed fog emitting behind some objects

---
+++ UnityProject/Assets/FogController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogController : MonoBehaviour
+{
+    void Start()
+    {
+        particleSystem.renderer.sortingLayerName = "Particles";
+    }
+}
