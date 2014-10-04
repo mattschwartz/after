9c153bd473f465961b752cdb96a80e917298eafd
Matt Schwartz <schwamat@gmail.com>
2014-10-04 12:23:36 -0500

    Elevator call animation works

---
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/Interactables/Transitions/ElevatorCallAnimationTransition.cs
@@ -0,0 +1,15 @@
+﻿using After.Interactable.Transitions;
+using After.Interactable;
+using UnityEngine;
+
+public class ElevatorCallAnimationTransition : PlayerLockTransition
+{
+    public override void Read(StateType fromState, StateType toState)
+    {
+        Player.transform.position = new Vector2(transform.position.x - 2.43f, Player.transform.position.y);
+        Player.SendMessage("LockPlayer");
+        Invoke("FreePlayer", LockDuration);
+
+        PlayerAnimator.SetTrigger("ElevatorCall");
+    }
+}
