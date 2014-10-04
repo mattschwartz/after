890e5efb93fd4765f024bd5d006becdcde15441f
Matt Schwartz <schwamat@gmail.com>
2014-10-04 13:56:32 -0500

    Like.. dark and stuff

---
+++ UnityProject/Assets/Scripts/ProximityTrigger/ProximityTransitions/ObservationProximityTransition.cs
@@ -0,0 +1,25 @@
+﻿using After.Interactable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace After.ProximityTrigger.ProximityTransitions
+{
+    public class ObservationProximityTransition : ProximityTransition
+    {
+        public string Observations;
+        private GameObject PlayerThoughts;
+
+        void Start()
+        {
+            PlayerThoughts = GameObject.Find("Player Thoughts");
+        }
+
+        public override void Read(StateType fromState, StateType toState)
+        {
+            PlayerThoughts.SendMessage("SetThought", Observations);
+        }
+    }
+}
