493e73c68ab397f0c5b43c4308d574948bb6d802
pixley <tylerpixley@gmail.com>
2014-10-05 17:17:56 -0500

    Moving my fader work over to my PC

--- UnityProject/Assets/Scripts/SceneFaderController.cs
+++ UnityProject/Assets/Scripts/SceneFaderController.cs
@@ -4,41 +4,29 @@ using System.Collections;
 public class SceneFaderController : MonoBehaviour {
 	public float FadeRate;
 	public int NextLevel;
-	private bool FadingOut;
-	private bool FadingIn;
+	public GUITexture guiTexture;
 
 	// Use this for initialization
 	void Start () {
-		FadingOut = false;
-		FadingIn = true;
 		// Set the texture so that it is the the size of the screen and covers it.
         guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
 	}
 
-	void FadeOut ()
+	public IEnumerator FadeOut ()
 	{
-		FadingOut = true;
+		// Lerp the colour of the texture between itself and black.
+    	guiTexture.color = Color.Lerp(guiTexture.color, Color.black, FadeRate * Time.deltaTime);
+    	yield return 1.0f / FadeRate;
+    	if (NextLevel == 0)
+    		Application.Quit();
+    	else
+    		Application.LoadLevel(NextLevel);
+    	yield return 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (FadingOut)
-		{
-			// Lerp the colour of the texture between itself and black.
-        	guiTexture.color = Color.Lerp(guiTexture.color, Color.black, FadeRate * Time.deltaTime);
-        	if (guiTexture.color.a <= .95f)
-        	{
-        		if (NextLevel == 0)
-        			Application.Quit();
-        		Application.LoadLevel(NextLevel);
-        	}
-		}
-		else if (FadingIn)
-		{
-			// Lerp the colour of the texture between itself and transparent.
-        	guiTexture.color = Color.Lerp(guiTexture.color, Color.clear, FadeRate * Time.deltaTime);
-        	if (guiTexture.color.a <= .05f)
-        		FadingIn = false;
-		}
+
+	public IEnumerator FadeIn ()
+	{
+		guiTexture.color = Color.Lerp(guiTexture.color, Color.clear, FadeRate * Time.deltaTime);
+		yield return 1.0f / FadeRate;
 	}
 }
