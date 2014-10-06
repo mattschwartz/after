56d7b157935bd2ec7d6fd75a28f2d68d69660449
Matt Schwartz <schwamat@gmail.com>
2014-10-06 12:56:18 -0500

    NextLevel changed to a string because int is unreliable

--- UnityProject/Assets/Scripts/SceneFaderController.cs
+++ UnityProject/Assets/Scripts/SceneFaderController.cs
@@ -1,51 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SceneFaderController : MonoBehaviour {
-	public float FadeRate;
-	public int NextLevel;
-	public GUITexture guiTexture;
+public class SceneFaderController : MonoBehaviour
+{
+    public float FadeRate;
+    public string NextLevel;
+    public GUITexture guiTexture;
 
-	// Use this for initialization
-	void Start () {
-		// Set the texture so that it is the the size of the screen and covers it.
+    // Use this for initialization
+    void Start()
+    {
+        // Set the texture so that it is the the size of the screen and covers it.
         guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
         guiTexture.enabled = true;
         StartCoroutine(FadeIn());
-	}
+    }
 
-	public IEnumerator FadeOut ()
-	{
-        guiTexture.enabled = true; ;
-		// Lerp the colour of the texture between itself and black.
-        while (true)
-        {
+    public IEnumerator FadeOut()
+    {
+        guiTexture.enabled = true;
+        // Lerp the colour of the texture between itself and black.
+        while (true) {
             guiTexture.color = Color.Lerp(guiTexture.color, Color.black, FadeRate * Time.deltaTime);
             yield return null;
-            if (guiTexture.color.a >= .95)
-            {
+            if (guiTexture.color.a >= .95) {
                 guiTexture.color = Color.black;
-                if (NextLevel == 0)
+                if (string.IsNullOrEmpty(NextLevel))
                     Application.Quit();
                 else
                     Application.LoadLevel(NextLevel);
                 break;
             }
         }
-	}
+    }
 
-	public IEnumerator FadeIn ()
-	{
-        while (true)
-        {
+    public IEnumerator FadeIn()
+    {
+        while (true) {
             guiTexture.color = Color.Lerp(guiTexture.color, Color.clear, FadeRate * Time.deltaTime);
             yield return null;
-            if (guiTexture.color.a <= .05)
-            {
+            if (guiTexture.color.a <= .05) {
                 guiTexture.color = Color.clear;
                 guiTexture.enabled = false;
                 break;
             }
         }
-	}
+    }
 }
