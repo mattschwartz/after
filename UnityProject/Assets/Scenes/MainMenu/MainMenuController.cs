c524eceff2fe9ddd471bac4973f5c356052830d0
Matt Schwartz <schwamat@gmail.com>
2014-10-08 02:32:45 -0500

    Code freeze

--- UnityProject/Assets/Scenes/MainMenu/MainMenuController.cs
+++ UnityProject/Assets/Scenes/MainMenu/MainMenuController.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class MainMenuController : MonoBehaviour
 {
-
+    public SceneFaderController Fader;
     private KeyCode ContinueButton = KeyCode.E;
 
     void Update()
     {
         if (Input.GetKeyDown(ContinueButton)) {
-            Application.LoadLevel("SewerLevel");
+            StartCoroutine(Fader.FadeOut());
         }
     }
 }
