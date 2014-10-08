d6f94d61f5f2900e19a8bba3c3e244db8687ae64
mattschwartz <mattschwartz@utexas.edu>
2014-10-08 16:55:40 -0500

    Fixed manhole position

--- UnityProject/Assets/Scenes/IntroLevel/Scripts/Transitions/ManholeAnimationTransition.cs
+++ UnityProject/Assets/Scenes/IntroLevel/Scripts/Transitions/ManholeAnimationTransition.cs
@@ -6,8 +6,8 @@ using After.Interactable.Transitions;
 public class ManholeAnimationTransition : PlayerLockTransition
 {
     public Animator ManholeAnimator;
-    private Vector3 LeftStandingCoords = new Vector3(17.55f, -11.12682f, 0);
-    private Vector3 RightStandingCoords = new Vector3(18.9f, -11.12682f, 0);
+    private Vector3 LeftStandingCoords = new Vector3(10.8f, -11.12682f, 0);
+    private Vector3 RightStandingCoords = new Vector3(12.1f, -11.12682f, 0);
 
     public override void Read(StateType fromState, StateType toState)
     {
