3940891ecff4f4d412bce2f65e1f7a8b94e715cc
mattschwartz <mattschwartz@utexas.edu>
2014-10-08 17:38:54 -0500

    Added press button to skip for cutscene

---
+++ UnityProject/Assets/Scenes/CutScenes/BridgeCrossing/CutSceneController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutSceneController : MonoBehaviour 
+{
+	public SceneFaderController Fader;
+
+	void Update() 
+	{
+		if (Input.anyKey) {
+			LoadNextLevel();
+		}
+	}
+
+	public void LoadNextLevel() 
+	{
+		StartCoroutine(Fader.FadeOut());
+	}
+}
