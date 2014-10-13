7cbbc6b7df154c83e976534d3f4e63ca04c38c67
mattschwartz <mattschwartz@utexas.edu>
2014-10-13 09:47:29 -0500

    Working on particles

---
+++ UnityProject/Assets/Scripts/ParticleRenderingController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleRenderingController : MonoBehaviour 
+{
+    void Start()
+    {
+        particleSystem.renderer.sortingLayerName = "Particles";
+    }
+}
