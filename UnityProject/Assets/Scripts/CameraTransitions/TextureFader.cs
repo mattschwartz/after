6ad38146af800b12ee0f1f1e34b07d2427cb30c8
mattschwartz <mattschwartz@utexas.edu>
2014-10-18 12:27:33 -0500

    Added ability to change textures in TextureFader

--- UnityProject/Assets/Scripts/CameraTransitions/TextureFader.cs
+++ UnityProject/Assets/Scripts/CameraTransitions/TextureFader.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace After.CameraTransitions
 {
+	/// <summary>TextureFader is a Singleton that can be used to fade a default
+	/// <para>texture or supplied texture over a specified duration</para>
+	/// </summary>
 	public class TextureFader : ScriptableObject
 	{
 		public GUITexture SwatchTexture;
@@ -13,6 +16,7 @@ namespace After.CameraTransitions
 		private float Duration;
 		private Color StartColor;
 		private Color EndColor;
+		private Texture DefaultTexture;
 
 		private TextureFader()
 		{
@@ -25,13 +29,11 @@ namespace After.CameraTransitions
 	        Instance.SwatchTexture.transform.position = new Vector3(0, 0, 0);
 	        Instance.SwatchTexture.pixelInset = new Rect(0, 0, Screen.width, Screen.height);
             Instance.SwatchTexture.enabled = false;
+            Instance.DefaultTexture = Instance.SwatchTexture.texture;
 		}
 
 		void Update()
 		{
-			if (Input.GetKeyDown(KeyCode.I)) {
-				Instance.Fade(Color.white, Color.clear, 1.85f);
-			}
 			if (!Instance.Fading) { return; }
 
 			float x = Instance.t / Instance.Duration;
@@ -39,12 +41,26 @@ namespace After.CameraTransitions
 
 			Instance.t += Time.deltaTime;
 
+			// Cleanup
 			if (Instance.t >= Instance.Duration) {
 				Instance.Fading = false;
 				Instance.SwatchTexture.enabled = false;
+				Instance.SwatchTexture.texture = Instance.DefaultTexture;
 			}
 		}
 
+		// Change the texture to something else for 1 fade
+		public void SetTexture(Texture texture)
+		{
+			SwatchTexture.texture = texture;
+		}
+
+		/// <summary>Fade the current texture from a starting color to an
+		/// <para>ending color</para>
+		/// <param name="start">The beginning color of the texture</param>
+		/// <param name="end">The ending color of the texture</param>
+		/// <param name="duration">The length of the fade transition (default 1)</param>
+		/// </summary>
 		public void Fade(Color start, Color end, float duration = 1)
 		{
 			Fading = true;
