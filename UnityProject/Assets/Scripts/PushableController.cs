f4f80ebb9385b9979f8f4dfa383cce0978d68b9b
Tyler Pixley <tylerpixley@gmail.com>
2014-10-21 12:54:50 -0500

    Hopeful fix for Egyptian Walk
    Taylor, you should be able to hook the push animation back in.

--- UnityProject/Assets/Scripts/PushableController.cs
+++ UnityProject/Assets/Scripts/PushableController.cs
@@ -2,7 +2,8 @@
     public float Drag;
     public float Mass;	// Use this for initialization	void Start () {
         RBody.drag = 90000f;
-        RBody.mass = 90000f;	}    //these two methods make it so that the player can only push the box while grounded,    //assuming the trigger is short and low enough.  Also triggers push animation.    //NOT YET WORKING    void OnTriggerEnter2D ()    {
+        RBody.mass = 90000f;
+        PlayerCon.Push(false);	}    //these two methods make it so that the player can only push the box while grounded,    //assuming the trigger is short and low enough.  Also triggers push animation.    //NOT HOOKED UP    void OnTriggerEnter2D ()    {
         RBody.drag = Drag;
         RBody.mass = Mass;        PlayerCon.Push(true);    }    void OnTriggerExit2D ()    {
         RBody.drag = 90000f;
