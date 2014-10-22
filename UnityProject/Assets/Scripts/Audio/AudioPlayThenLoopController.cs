450c3e28d9553f837adab32d50e36b11ee9c4726
mattschwartz <mattschwartz@utexas.edu>
2014-10-22 16:57:17 -0500

    Added PlayThenLoopController

---
+++ UnityProject/Assets/Scripts/Audio/AudioPlayThenLoopController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace After.Audio 
+{
+	public class AudioPlayThenLoopController : MonoBehaviour 
+	{
+
+		public AudioClip OneShotClip;
+		public AudioClip LoopClip;
+
+		void Start() 
+		{
+			StartCoroutine(PlayThenLoop());
+		}
+
+		private IEnumerator PlayThenLoop()
+		{
+			AudioManager.PlayClipAtPoint(OneShotClip, transform.position);
+			yield return new WaitForSeconds(OneShotClip.length);
+			AudioManager.LoopClipAtPoint(LoopClip, transform.position);
+		}
+	}	
+}
+
