80b419b5e41433b89c54ce3831bdc5e8965d5f9a
Tyler Pixley <tylerpixley@gmail.com>
2014-10-29 15:47:51 -0500

    Elevator Cutscene 1 has music and collision now

--- UnityProject/Assets/Scripts/PushableController.cs
+++ UnityProject/Assets/Scripts/PushableController.cs
@@ -1,6 +1,38 @@
-﻿using UnityEngine;using System.Collections;public class PushableController : MonoBehaviour {    public PlayerController PlayerCon;    public Rigidbody2D RBody;
-    public BoxCollider2D Trigger;    public float Drag;    public float Mass;
-    private bool Falling;	// Use this for initialization	void Start () {        RBody.drag = 90000f;        RBody.mass = 90000f;        PlayerCon.Push(false);	}    //these two methods make it so that the player can only push the box while grounded,    //assuming the trigger is short and low enough.  Also triggers push animation.    void OnTriggerEnter2D ()    {        RBody.drag = Drag;        RBody.mass = Mass;        PlayerCon.Push(true);    }    void OnTriggerExit2D ()    {        RBody.drag = 90000f;        RBody.mass = 90000f;        PlayerCon.Push(false);    }    void Update ()
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushableController : MonoBehaviour {
+    public PlayerController PlayerCon;
+    public Rigidbody2D RBody;
+    public BoxCollider2D Trigger;
+    public float Drag;
+    public float Mass;
+    private bool Falling;
+
+	// Use this for initialization
+	void Start () {
+        RBody.drag = 90000f;
+        RBody.mass = 90000f;
+        PlayerCon.Push(false);
+	}
+
+    //these two methods make it so that the player can only push the box while grounded,
+    //assuming the trigger is short and low enough.  Also triggers push animation.
+    void OnTriggerEnter2D ()
+    {
+        RBody.drag = Drag;
+        RBody.mass = Mass;
+        PlayerCon.Push(true);
+    }
+
+    void OnTriggerExit2D ()
+    {
+        RBody.drag = 90000f;
+        RBody.mass = 90000f;
+        PlayerCon.Push(false);
+    }
+
+    void Update ()
     {
         if (RBody.velocity.y != 0f)
         {
@@ -19,4 +51,5 @@
             Falling = false;
             RBody.drag = 90000f;
         }
-    }}
+    }
+}
