cd778fa949fe1a07886b0d743d172f02b7d196bc
mattschwartz <mattschwartz@utexas.edu>
2014-10-29 14:42:15 -0500

    Working on boulder physics

---
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/Interactables/Transitions/RockMoveTransition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Interactable;
+using After.Interactable.Transitions;
+
+public class RockMoveTransition : Transition 
+{
+	public Rigidbody2D RockBody;
+	public Vector2 AppliedForce;
+
+	public override void Read(StateType fromState, StateType toState)
+	{
+		Debug.Log("Adding some force to this bitch.");
+		RockBody.AddForce(AppliedForce);
+	}
+}
