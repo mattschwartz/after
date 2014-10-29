6dd5f654d6bc8f644cbe7dc79418535c741120a2
Matt Schwartz <schwamat@gmail.com>
2014-10-29 01:48:30 -0500

    Pause Menu has been arted.

--- UnityProject/Assets/Scripts/Gui/OptionsMenuController.cs
+++ UnityProject/Assets/Scripts/Gui/OptionsMenuController.cs
@@ -1,69 +1,137 @@
 using System.Collections;
 using After.Audio;
 using UnityEngine;
+using After.Scene.SceneManagement;
 
 namespace After.Gui
 {
-	public class OptionsMenuController : MonoBehaviour
-	{
-		#region Members
+    public class OptionsMenuController : MonoBehaviour
+    {
+        #region Members
 
-		public float hSliderValue;
-		public GUIStyle opaCustomStyle;
-		public GUIStyle SliderStyle;
-		public GUIStyle SliderThumbStyle;
+        public float hSliderValue;
+        public GUIStyle opaCustomStyle;
+        public GUIStyle SliderStyle;
+        public GUIStyle SliderThumbStyle;
+        public Texture JournalIconTexture;
+        public Texture MainMenuIconTexture;
+        public Texture MenuBackgroundTexture;
 
-		private KeyCode kcOkay = KeyCode.Return;
-		private KeyCode kcCancel = KeyCode.Escape;
-		private Rect SoundSliderBounds;
-		private Rect btnBoundsOK;
-		private Rect btnBoundsCancel;
+        private bool Visible = false;
+        private KeyCode kcOkay = KeyCode.Return;
+        private KeyCode kcCancel = KeyCode.Escape;
+        private Rect MenuBackgroundBounds;
+        private Rect SoundSliderBounds;
+        private Rect JournalIconBounds;
+        private Rect MainMenuBounds;
+        private Rect TitleBounds;
+        private Rect LabelBounds;
 
-		#endregion
+        #endregion
 
-		void Start()
-		{
-			Resize();
-		}
+        void Start()
+        {
+            hSliderValue = AudioListener.volume * 100f;
+            Resize();
+        }
 
-		void Update()
-		{
-			ProcessKeyboard();
-			ProcessMouse();
-			Resize();
-			AudioManager.SetVolume(hSliderValue / 100f);
-		}
+        void Update()
+        {
+            if (!Visible && SceneHandler.GUILock) { return; }
 
-		private void ProcessKeyboard()
-		{
+            ProcessKeyboard();
 
-		}
+            if (!Visible) { return; }
 
-		private void ProcessMouse()
-		{
+            Resize();
+            AudioManager.SetVolume(hSliderValue / 100f);
+        }
 
-		}
+        private void ProcessKeyboard()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                Visible = !Visible;
+                SceneHandler.GUILock = Visible;
+                Time.timeScale = Visible ? 0 : 1;
+            }
+        }
 
-		private void Resize()
-		{
+        private void Resize()
+        {
             var screenCoords = new Vector3(0.5f, 0.5f, 0);
             var camPos = Camera.main.ViewportToScreenPoint(screenCoords);
 
-			SoundSliderBounds = new Rect {
-				x = camPos.x,
-				y = camPos.y,
-				width = 250,
-				height = 25
-			};	
-		}
+            SoundSliderBounds = new Rect {
+                x = camPos.x,
+                y = camPos.y,
+                width = 175,
+                height = 25
+            };
 
-		void OnGui()
-		{
-			hSliderValue = GUI.HorizontalSlider(SoundSliderBounds, 
-				hSliderValue, 0, 100, SliderStyle, SliderThumbStyle);
+            screenCoords = new Vector3(0, 1, 0);
+            camPos = Camera.main.ViewportToScreenPoint(screenCoords);
 
-			GUI.Label(btnBoundsOK, "[Enter] Apply", opaCustomStyle);
-			GUI.Label(btnBoundsCancel, "[Escape] Cancel", opaCustomStyle);
-		}
-	}
+            JournalIconBounds = new Rect {
+                x = camPos.x,
+                y = camPos.y - JournalIconTexture.height,
+                width = JournalIconTexture.width,
+                height = JournalIconTexture.height
+            };
+
+            screenCoords = new Vector3(1, 1, 0);
+            camPos = Camera.main.ViewportToScreenPoint(screenCoords);
+
+            MainMenuBounds = new Rect {
+                x = camPos.x - MainMenuIconTexture.width,
+                y = camPos.y - MainMenuIconTexture.height,
+                width = MainMenuIconTexture.width,
+                height = MainMenuIconTexture.height
+            };
+
+            screenCoords = new Vector3(0.5f, 0.1f, 0);
+            camPos = Camera.main.ViewportToScreenPoint(screenCoords);
+
+            TitleBounds = new Rect {
+                x = camPos.x,
+                y = camPos.y,
+                width = 0,
+                height = 0
+            };
+
+            screenCoords = new Vector3(0.4f, 0.5f, 0);
+            camPos = Camera.main.ViewportToScreenPoint(screenCoords);
+
+            LabelBounds = new Rect {
+                x = camPos.x,
+                y = camPos.y,
+                width = 0,
+                height = 0
+            };
+
+            screenCoords = new Vector3(0.4f, 0.5f, 0);
+            camPos = Camera.main.ViewportToScreenPoint(screenCoords);
+
+            MenuBackgroundBounds = new Rect {
+                x = 0,
+                y = 0,
+                width = Screen.width,
+                height = Screen.height
+            };
+        }
+
+        void OnGUI()
+        {
+            if (!Visible) { return; }
+
+            GUI.DrawTexture(MenuBackgroundBounds, MenuBackgroundTexture);
+
+            hSliderValue = GUI.HorizontalSlider(SoundSliderBounds,
+                hSliderValue, 0, 100, SliderStyle, SliderThumbStyle);
+
+            GUI.Label(TitleBounds, "Game Options", opaCustomStyle);
+            GUI.Label(LabelBounds, "Game Volume:", opaCustomStyle);
+            GUI.DrawTexture(MainMenuBounds, MainMenuIconTexture);
+            GUI.DrawTexture(JournalIconBounds, JournalIconTexture);
+        }
+    }
 }
--- UnityProject/Assets/Scripts/Interactable/HeldItemController.cs
+++ UnityProject/Assets/Scripts/Interactable/HeldItemController.cs
@@ -10,32 +10,32 @@ namespace After.Interactable
     {
         #region Members
 
-        public float ItemHeldSize = 100;
-        public float BackpackSize = 175;
+        private float ItemHeldSize = 100;
+        private float BackpackSize = 175;
         public GameObject Player;
         public Texture BackpackTextureEmpty;
         public Texture BackpackTextureFull;
 
         public GameObject ItemHeld { get; private set; }
         private Texture ItemHeldTexture;
+        private Rect BackpackPosition;
+        private Rect ItemPosition;
 
         #endregion
+
+        void Start()
+        {
+            var camPos = Camera.main.ViewportToScreenPoint(new Vector3(1, 1, 0));
+            BackpackPosition = new Rect(camPos.x - BackpackSize, camPos.y - BackpackSize, BackpackSize, BackpackSize);
+        }
 
         void OnGUI()
         {
             if (SceneHandler.GUILock) { return; }
 
-            var camPos = Camera.main.ViewportToScreenPoint(new Vector3(1, 1, 0));
-            Rect BackpackPosition = new Rect(camPos.x - BackpackSize, camPos.y - BackpackSize, BackpackSize, BackpackSize);
-
             if (ItemHeld != null) {
                 GUI.DrawTexture(BackpackPosition, BackpackTextureFull);
-
-                float scale = ItemHeldSize / Mathf.Max(ItemHeldTexture.width, ItemHeldTexture.height);
-                float itemWidth = ItemHeldTexture.width * scale;
-                float itemHeight = ItemHeldTexture.height * scale;
-                Rect itemPosition = new Rect(camPos.x - itemWidth - (BackpackSize - itemWidth) / 2, camPos.y - itemHeight - (BackpackSize - itemHeight) / 2, itemWidth, itemHeight);
-                GUI.DrawTexture(itemPosition, ItemHeldTexture);
+                GUI.DrawTexture(ItemPosition, ItemHeldTexture);
             } else {
                 GUI.DrawTexture(BackpackPosition, BackpackTextureEmpty);
             }
@@ -52,11 +52,12 @@ namespace After.Interactable
 
             ItemHeldTexture = ItemHeld.GetComponent<SpriteRenderer>().sprite.texture;
             SceneHandler.CurrentPlayer.ItemHeld = ItemHeld.name;
-        }
 
-        public void ShowItemHeld()
-        {
-            Debug.LogWarning("ShowItemHeld is deprecated.");
+            var camPos = Camera.main.ViewportToScreenPoint(new Vector3(1, 1, 0));
+            float scale = ItemHeldSize / Mathf.Max(ItemHeldTexture.width, ItemHeldTexture.height);
+            float itemWidth = ItemHeldTexture.width * scale;
+            float itemHeight = ItemHeldTexture.height * scale;
+            ItemPosition = new Rect(camPos.x - itemWidth - (BackpackSize - itemWidth) / 2, camPos.y - itemHeight - (BackpackSize - itemHeight) / 2, itemWidth, itemHeight);
         }
 
         public void DropItem()
