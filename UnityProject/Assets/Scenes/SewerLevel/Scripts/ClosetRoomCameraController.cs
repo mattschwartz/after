842b1457dd96f53ea5487a6160bc9158fd0c637f
Matt Schwartz <schwamat@gmail.com>
2014-11-04 02:42:28 -0600

    Triggers just ain't havin' it

--- UnityProject/Assets/Scenes/SewerLevel/Scripts/ClosetRoomCameraController.cs
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/ClosetRoomCameraController.cs
@@ -7,33 +7,32 @@ public class ClosetRoomCameraController : MonoBehaviour
 
     public Transform CenterCameraOnTransform;
 
+    private bool PlayerEntered = false;
+
     #endregion
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.name != "Player") {
-            return;
+        if (other.name == "Player") {
+            PlayerEntered = true;
+            Camera.main.SendMessage("SetStaticCamera", true);
         }
-        Camera.main.SendMessage("SetStaticCamera", true);
-    }
-
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.name != "Player") {
-            return;
-        }
-
-        var pos = CenterCameraOnTransform.position;
-        pos.z = Camera.main.transform.position.z;
-        Camera.main.transform.position = pos;
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.name != "Player") {
-            return;
+        if (other.name == "Player") {
+            PlayerEntered = false;
+            Camera.main.SendMessage("SetStaticCamera", false);
         }
-        
-        Camera.main.SendMessage("SetStaticCamera", false);
+    }
+
+    void Update()
+    {
+        if (!PlayerEntered) { return; }
+
+        var pos = CenterCameraOnTransform.position;
+        pos.z = Camera.main.transform.position.z;
+        Camera.main.transform.position = pos;
     }
 }
--- UnityProject/Assets/Scenes/SewerLevel/Scripts/PlayerScalerController.cs
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/PlayerScalerController.cs
@@ -7,17 +7,19 @@ public class PlayerScalerController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.name != "Player") { return; }
-        var scale = other.transform.localScale;
+        if (other.name == "Player") {
+            var scale = other.transform.localScale;
 
-        OriginalTransform = new Vector2(Mathf.Abs(scale.x), scale.y);
-        other.transform.localScale = new Vector2(1.5f * Mathf.Sign(scale.x), 1.5f);
+            OriginalTransform = new Vector2(Mathf.Abs(scale.x), scale.y);
+            other.transform.localScale = new Vector2(1.5f * Mathf.Sign(scale.x), 1.5f);
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.name != "Player") { return; }
-        var scale = other.transform.localScale;
-        other.transform.localScale = new Vector2(OriginalTransform.x * Mathf.Sign(scale.x), OriginalTransform.y);
+        if (other.name == "Player") {
+            var scale = other.transform.localScale;
+            other.transform.localScale = new Vector2(OriginalTransform.x * Mathf.Sign(scale.x), OriginalTransform.y);
+        }
     }
 }
