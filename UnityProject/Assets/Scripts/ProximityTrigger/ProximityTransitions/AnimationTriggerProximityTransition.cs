39b86126f2c163447f82386df7724d756bd87e07
Matt Schwartz <schwamat@gmail.com>
2014-11-04 16:32:08 -0600

    Added ghostly figure and rock pry animation.

---
+++ UnityProject/Assets/Scripts/ProximityTrigger/ProximityTransitions/AnimationTriggerProximityTransition.cs
@@ -0,0 +1,16 @@
+﻿using After.Interactable;
+using UnityEngine;
+
+namespace After.ProximityTrigger.ProximityTransitions
+{
+    public class AnimationTriggerProximityTransition : ProximityTransition
+    {
+        public string Trigger;
+        public Animator Animator;
+
+        public override void Read(StateType fromState, StateType toState)
+        {
+            Animator.SetTrigger(Trigger);
+        }
+    }
+}
