73b4070eae5b671ce70fd827f06072a7a5bd4757
mattschwartz <mattschwartz@utexas.edu>
2014-11-05 16:27:31 -0600

    Added rockslide controller

---
+++ UnityProject/Assets/Scenes/PowerStationLevel/Scripts/RockSlideAudioController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockSlideAudioController : MonoBehaviour 
+{
+	public GameObject WatchFor;
+	public AudioSource SlideSource;
+
+	void OnCollisionStay2D(Collision2D other) 
+	{
+		if (other.gameObject.name != WatchFor.name) { return; }
+		float velocity = Mathf.Abs(WatchFor.rigidbody2D.velocity.x);
+
+		if (velocity > 0.01) {
+			if (!SlideSource.isPlaying) {
+				SlideSource.Play();
+			}
+		} else {
+			SlideSource.Stop();
+		}
+	}
+}
