38463ab4419739ff9494d4f64f6091c948656dac
Matt Schwartz <schwamat@gmail.com>
2014-11-05 01:24:59 -0600

    m

--- UnityProject/Assets/Scenes/TitleScreenLevel/Scripts/GuiController.cs
+++ UnityProject/Assets/Scenes/TitleScreenLevel/Scripts/GuiController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using After.Audio;
 
 public class GuiController : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class GuiController : MonoBehaviour
     public GUIStyle ContinueStyle;
     public GUIStyle OptionsStyle;
     public GUIStyle ExitStyle;
+    public AudioClip MousePressClip;
+    public SceneFaderController SceneFader;
 
     private float btnWidth = 123;
     private float btnHeight = 20;
@@ -33,18 +36,23 @@ public class GuiController : MonoBehaviour
 
     private void NewGame_Click()
     {
+        AudioManager.PlayClipAtPoint(MousePressClip, transform.position);
+        StartCoroutine(SceneFader.FadeOut());
     }
 
     private void Continue_Click()
     {
+        AudioManager.PlayClipAtPoint(MousePressClip, transform.position);
     }
 
     private void Options_Click()
     {
+        AudioManager.PlayClipAtPoint(MousePressClip, transform.position);
     }
 
     private void Exit_Click()
     {
+        AudioManager.PlayClipAtPoint(MousePressClip, transform.position);
         Application.Quit();
     }
 
