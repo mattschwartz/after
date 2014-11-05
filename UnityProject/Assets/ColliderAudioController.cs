1d8d6aca5478ac3bde87bdd0abf1bf256b7f05db
Matt Schwartz <schwamat@gmail.com>
2014-11-04 23:47:47 -0600

    Added rock hitting trapdoor sound fx

---
+++ UnityProject/Assets/ColliderAudioController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Audio;
+using System.Collections.Generic;
+
+public class ColliderAudioController : MonoBehaviour
+{
+    public float Volume = 1;
+    public GameObject WatchFor;
+    public List<AudioClip> AudioClips;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == WatchFor.name) {
+            AudioClips.ForEach(t => AudioManager.PlayClipAtPoint(t, transform.position, Volume));
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == WatchFor.name) {
+            AudioClips.ForEach(t => AudioManager.PlayClipAtPoint(t, transform.position, Volume));
+            Destroy(this.gameObject);
+        }
+    }
+}
