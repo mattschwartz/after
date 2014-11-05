43417075e9a6811849486bdb1e305403b5d8b2d6
mattschwartz <mattschwartz@utexas.edu>
2014-11-05 16:06:44 -0600

    Increased chair spawning fun by 10%

--- UnityProject/Assets/Scenes/SkyscraperLevel/Scripts/ChairController.cs
+++ UnityProject/Assets/Scenes/SkyscraperLevel/Scripts/ChairController.cs
@@ -5,7 +5,6 @@ public class ChairController : MonoBehaviour
 {
 	void OnCollisionEnter2D(Collision2D other) 
 	{
-		Debug.Log("Colliding with : " + other.gameObject.name);
 		if (other.gameObject.name == "Player") {
 			float xForce = Random.Range(-1000, 1000);
 			float yForce = Random.Range(-1000, 1000);
