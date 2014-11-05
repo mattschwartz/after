f381f05bf8a0823bd0723f21f37b58a98c65a06b
Matt Schwartz <schwamat@gmail.com>
2014-11-04 21:33:56 -0600

    Rock breaks trapdoor.
    But paper still beats rock??

---
+++ UnityProject/Assets/Scenes/PowerStationLevel/Scripts/EnableTrapdoorController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableTrapdoorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
