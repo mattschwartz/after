64985ff33c9a76287653ca282211ec7a58483ff6
mattschwartz <mattschwartz@utexas.edu>
2014-11-05 16:46:42 -0600

    Sound updates

--- UnityProject/Assets/Scenes/PowerStationLevel/Scripts/RockSlideAudioController.cs
+++ UnityProject/Assets/Scenes/PowerStationLevel/Scripts/RockSlideAudioController.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class RockSlideAudioController : MonoBehaviour 
 {
+	public float MaxVelocity = 0.948f;
 	public GameObject WatchFor;
 	public AudioSource SlideSource;
 
 	void OnCollisionStay2D(Collision2D other) 
 	{
+		if (WatchFor == null) { Destroy(this.gameObject); }
 		if (other.gameObject.name != WatchFor.name) { return; }
 		float velocity = Mathf.Abs(WatchFor.rigidbody2D.velocity.x);
+		
+		SlideSource.volume = velocity / MaxVelocity;
 
 		if (velocity > 0.01) {
 			if (!SlideSource.isPlaying) {
@@ -18,5 +22,12 @@ public class RockSlideAudioController : MonoBehaviour
 		} else {
 			SlideSource.Stop();
 		}
+	}
+
+	void OnCollisionExit2D(Collision2D other) 
+	{
+		if (other.gameObject.name != WatchFor.name) { return; }
+
+		SlideSource.Stop();
 	}
 }
