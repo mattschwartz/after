34974170492640162fd5f70a2d06ffb877e1299d
Matt Schwartz <schwamat@gmail.com>
2014-11-04 23:29:54 -0600

    Added console trigger.

---
+++ UnityProject/Assets/Scripts/Interactable/Transitions/AddEntryTransition.cs
@@ -0,0 +1,19 @@
+﻿using After.Interactable;
+using After.Journal;
+using UnityEngine;
+
+namespace After.Interactable.Transitions
+{
+    public class AddEntryTransition : Transition
+    {
+        public string EntryName;
+        public string Update;
+        public Texture Image;
+
+        public override void Read(StateType fromState, StateType toState)
+        {
+            var entry = new Entry(EntryName, Update, Image);
+            JournalController.Instance.AddEntry(entry);
+        }
+    }
+}
