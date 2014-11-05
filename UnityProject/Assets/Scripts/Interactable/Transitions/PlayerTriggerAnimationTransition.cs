42d54aaeea462f9c76063cdcfabd3781c8519dc6
Matt Schwartz <schwamat@gmail.com>
2014-11-04 23:08:38 -0600

    Added elevator to power station

---
+++ UnityProject/Assets/Scripts/Interactable/Transitions/PlayerTriggerAnimationTransition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using After.Interactable;
+using After.Interactable.Transitions;
+using UnityEngine;
+
+namespace After.Interactable.Transitions
+{
+    public class PlayerTriggerAnimationTransition : PlayerLockTransition
+    {
+        public float xOffset = 4;
+        public string Trigger;
+
+        public override void Read(StateType fromState, StateType toState)
+        {
+            Player.transform.position = new Vector2(transform.position.x - xOffset, transform.position.y);
+            Player.SendMessage("LockPlayer");
+            Invoke("FreePlayer", LockDuration);
+
+            PlayerAnimator.SetTrigger(Trigger);
+        }
+    }
+}
--- UnityProject/Assets/Scripts/Interactable/Transitions/TriggerAnimationTransition.cs
+++ UnityProject/Assets/Scripts/Interactable/Transitions/TriggerAnimationTransition.cs
@@ -1,25 +1,21 @@
-﻿using System;
+﻿using After.Interactable;
+using After.Interactable.Transitions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using After.Interactable;
-using After.Interactable.Transitions;
 using UnityEngine;
 
 namespace After.Interactable.Transitions
 {
-    public class TriggerAnimationTransition : PlayerLockTransition
+    public class TriggerAnimationTransition : Transition
     {
-        public float xOffset = 4;
         public string Trigger;
+        public Animator Animator;
 
         public override void Read(StateType fromState, StateType toState)
         {
-            Player.transform.position = new Vector2(transform.position.x - xOffset, transform.position.y);
-            Player.SendMessage("LockPlayer");
-            Invoke("FreePlayer", LockDuration);
-
-            PlayerAnimator.SetTrigger(Trigger);
+            Animator.SetTrigger(Trigger);
         }
     }
 }
