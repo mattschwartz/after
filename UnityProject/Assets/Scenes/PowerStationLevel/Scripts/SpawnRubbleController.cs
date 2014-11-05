9fe8e4c8ce41b1e6b317113860810beb66f6d906
Matt Schwartz <schwamat@gmail.com>
2014-11-04 21:45:17 -0600

    Adding rubble to power station

---
+++ UnityProject/Assets/Scenes/PowerStationLevel/Scripts/SpawnRubbleController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnRubbleController : MonoBehaviour
+{
+    public Rigidbody2D Rubble;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name != "Player") { return; }
+
+        Rubble.gravityScale = 8;
+
+        Destroy(this.gameObject);
+    }
+}
