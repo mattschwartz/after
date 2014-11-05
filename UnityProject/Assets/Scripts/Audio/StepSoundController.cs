9810c464182262262b8d1ab0bde9e6e2b3fe1eff
mattschwartz <mattschwartz@utexas.edu>
2014-11-05 17:05:56 -0600

    Added volume for step sound controller.

--- UnityProject/Assets/Scripts/Audio/StepSoundController.cs
+++ UnityProject/Assets/Scripts/Audio/StepSoundController.cs
@@ -7,12 +7,13 @@ public class StepSoundController : MonoBehaviour
 {
     #region Public Members
 
+	public float Volume = 1;
     public List<AudioClip> StepSounds;
 
     #endregion
 
-    public void PlayFootstep(float volume=1.0F)
+    public void PlayFootstep()
     {
-        AudioManager.PlayMaterialFootstepAtPoint(StepSounds, transform.position, volume);
+        AudioManager.PlayMaterialFootstepAtPoint(StepSounds, transform.position, Volume);
     }
 }
