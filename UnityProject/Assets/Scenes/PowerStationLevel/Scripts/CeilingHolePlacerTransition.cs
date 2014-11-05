6f54891b9abe706062daa228077406ee4e741ca6
Matt Schwartz <schwamat@gmail.com>
2014-11-05 00:25:17 -0600

    Hole is drawn now

---
+++ UnityProject/Assets/Scenes/PowerStationLevel/Scripts/CeilingHolePlacerTransition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Utility;
+using After.ProximityTrigger.ProximityTransitions;
+using After.Interactable;
+
+public class CeilingHolePlacerTransition : ProximityTransition
+{
+    #region Members
+
+    public SpriteRenderer CeilingHole;
+
+    #endregion
+
+    void Start()
+    {
+        CeilingHole.enabled = false;
+    }
+
+    public override void Read(StateType fromState, StateType toState)
+    {
+        CeilingHole.enabled = true;
+    }
+}
