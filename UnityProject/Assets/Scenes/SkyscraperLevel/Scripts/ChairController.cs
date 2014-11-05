8485e484a8dc123bf2b3c0348fc488996d67113b
mattschwartz <mattschwartz@utexas.edu>
2014-11-05 16:02:16 -0600

    Chairspawning fun.
    Fun for the whole family!

---
+++ UnityProject/Assets/Scenes/SkyscraperLevel/Scripts/ChairController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChairController : MonoBehaviour 
+{
+	void OnCollisionEnter2D(Collision2D other) 
+	{
+		Debug.Log("Colliding with : " + other.gameObject.name);
+		if (other.gameObject.name == "Player") {
+			float xForce = Random.Range(-1000, 1000);
+			float yForce = Random.Range(-1000, 1000);
+			rigidbody2D.AddForce(new Vector2(xForce, yForce));
+		}
+	}
+}
