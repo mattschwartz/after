c97f2d69fbe3ab1b60dadd70a3352c9bb893af7f
Matt Schwartz <schwamat@gmail.com>
2014-11-06 22:30:25 -0600

    Player tries to move boulder in sewer level at first

---
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/Interactables/Transitions/RockQuickPushAnimationTransition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Interactable;
+using After.Interactable.Transitions;
+
+public class RockQuickPushAnimationTransition : Transition 
+{
+    public float Duration;
+    public string Boolean;
+    public Animator PlayerAnimator;
+    public PlayerController Player;
+
+    public override void Read(StateType fromState, StateType toState)
+    {
+        StartCoroutine(QuickPush());
+    }
+
+    private IEnumerator QuickPush()
+    {
+        Player.LockPlayer();
+        PlayerAnimator.SetBool(Boolean, true);
+        yield return new WaitForSeconds(Duration);
+        PlayerAnimator.SetBool(Boolean, false);
+        Player.FreePlayer();
+    }
+}
