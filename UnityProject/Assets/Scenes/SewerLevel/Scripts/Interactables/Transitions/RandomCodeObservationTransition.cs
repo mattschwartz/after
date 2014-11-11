41e79ac209fa9b874d4b83e6ddd192abb644f393
Matt Schwartz <schwamat@gmail.com>
2014-11-10 18:11:04 -0600

    As if the sewer level wasn't polished enough.

---
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/Interactables/Transitions/RandomCodeObservationTransition.cs
@@ -0,0 +1,27 @@
+﻿using After.Interactable;
+using After.Interactable.Transitions;
+using UnityEngine;
+using System.Collections;
+
+public class RandomCodeObservationTransition : Transition
+{
+    private GameObject PlayerThoughts;
+
+    void Start()
+    {
+        PlayerThoughts = GameObject.Find("Player Thoughts");
+    }
+
+    public override void Read(StateType fromState, StateType toState)
+    {
+        int tryCode;
+        string observations = "A numeric code, hmm... ";
+
+        do {
+            tryCode = Random.Range(1000, 9999);
+        } while (tryCode == 4239);
+
+        observations += tryCode + "... Nope.";
+        PlayerThoughts.SendMessage("SetThought", observations);
+    }
+}
