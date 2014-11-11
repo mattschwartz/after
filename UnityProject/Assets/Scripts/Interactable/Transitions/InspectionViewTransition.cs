8fed3829a39a72b494436fcbdf31d4ea1f9ec282
Matt Schwartz <schwamat@gmail.com>
2014-11-10 23:02:27 -0600

    Added News Stand to intro level.

---
+++ UnityProject/Assets/Scripts/Interactable/Transitions/InspectionViewTransition.cs
@@ -0,0 +1,26 @@
+﻿using After.Interactable.Transitions;
+using After.Interactable;
+using UnityEngine;
+using System.Collections;
+
+namespace After.Interactable.Transitions
+{
+    public class InspectionViewTransition : Transition
+    {
+        #region Members
+
+        public bool AddToJournal;
+        public float ScreenSize;
+        public string Title;
+        public string Observations;
+        public Texture InspectingTexture;
+
+        #endregion
+
+        public override void Read(StateType fromState, StateType toState)
+        {
+            float size = (ScreenSize / 100f) * (float)Screen.width;
+            InspectorController.Instance.InspectItem(Title, Observations, InspectingTexture, size, AddToJournal);
+        }
+    }
+}
