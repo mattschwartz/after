f0313dc108d8dac704a85f1abc8b401a6eb55f84
rluck0419 <rluck0419@gmail.com>
2014-11-12 15:25:25 -0600

    reorganized sound & added title music & script

---
+++ UnityProject/Assets/Scenes/TitleScreenLevel/Scripts/ClickPitchController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RClickPitchController : MonoBehaviour {
+
+// values of notes (added to root pitch, 1)
+//a = 0.05946309435905
+//b = 0.12246204830885
+//c = 0.1892071150019
+//d = 0.2599210498937
+//e = 0.3348398541685
+//f = 0.4142135623711
+
+// randomly select pitch to add to root pitch (1)
+// +/- a-f
+// pitch = 1 + Random([a,b,c,d,e,f, a.neg,b.neg,c.neg,d.neg,e.neg,f.neg])
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
