2588d0b92d13bd3254c5cb46c642c0488a0db7ff
Matt Schwartz <schwamat@gmail.com>
2014-11-11 23:28:12 -0600

    Player walks and dies in skyscraper.

---
+++ UnityProject/Assets/Scenes/SkyscraperLevel/Scripts/PlayerRunTransition.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Interactable;
+using After.Interactable.Transitions;
+
+public class PlayerRunTransition : Transition
+{
+    #region Members
+
+    public float Speed;
+    public PlayerController Player;
+    public Animator PlayerAnimator;
+
+    private bool Active;
+
+    #endregion
+
+    void Start()
+    {
+        Active = false;
+    }
+
+    public override void Read(StateType fromState, StateType toState)
+    {
+        Player.LockPlayer();
+        PlayerAnimator.SetFloat("Velocity", 1);
+        Active = true;
+    }
+
+    void Update()
+    {
+        if (Active) {
+            Player.rigidbody2D.velocity = new Vector2(Speed, Player.rigidbody2D.velocity.y);
+        }
+    }
+
+}
