547ef9f7b4227790142905a7c614e9d66d0b6f10
mattschwartz <mattschwartz@utexas.edu>
2014-11-19 17:04:38 -0600

    Stuff

---
+++ UnityProject/Assets/Scenes/SkyscraperLevel/Scripts/DropBoulderTransition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Interactable;
+using After.Interactable.Transitions;
+
+public class DropBoulderTransition : Transition 
+{
+	public Rigidbody2D BoulderBody;
+
+    public override void Read(StateType fromState, StateType toState)
+    {
+    	BoulderBody.gravityScale = 8;
+    }
+}
