9ec6acd366bed6bd7a6768a4923c1195152fd828
Matt Schwartz <schwamat@gmail.com>
2014-11-18 18:47:04 -0600

    Ghostly figure appears on corpse too.

---
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/Interactables/Transitions/ShowAndDissipateGhostlyFigureTransition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Interactable.Transitions;
+using After.Interactable;
+
+public class ShowAndDissipateGhostlyFigureTransition : TriggerAnimationTransition
+{
+    #region Members
+
+    public SpriteRenderer Renderer;
+
+    #endregion
+
+    void Start()
+    {
+        Animator.enabled = false;
+        Renderer.enabled = false;
+    }
+
+    public override void Read(StateType fromState, StateType toState)
+    {
+        Animator.enabled = true;
+        Renderer.enabled = true;
+        Animator.SetTrigger(Trigger);
+    }
+}
