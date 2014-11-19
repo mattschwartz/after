521c1f168eb220e38e319140a44b4fd12ae41e47
mattschwartz <mattschwartz@utexas.edu>
2014-11-19 16:41:46 -0600

    Player yanks power cable now

---
+++ UnityProject/Assets/Scenes/SkyscraperLevel/Scripts/HideSpriteTransition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Interactable;
+using After.Interactable.Transitions;
+
+public class HideSpriteTransition : Transition
+{
+	public SpriteRenderer SpriteToHide;
+
+    public override void Read(StateType fromState, StateType toState)
+    {
+    	SpriteToHide.enabled = false;
+    }
+
+}
