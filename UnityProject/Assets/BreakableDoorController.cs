ffa60adea19775b4511096a8a6ac80be3e102109
Tyler Pixley <tylerpixley@gmail.com>
2014-11-19 12:02:49 -0600

    More Skyscraper functionality, some items added

---
+++ UnityProject/Assets/BreakableDoorController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableDoorController : MonoBehaviour {
+
+    public GameObject Target;
+    public BoxCollider2D DoorCollide;
+    public SpriteRenderer Sprite;
+    private bool Triggered;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == Target && !Triggered)
+        {
+            DoorCollide.isTrigger = true;
+            Sprite.sortingLayerName = "Background";
+            Sprite.sortingOrder = 0;
+            Triggered = true;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        Triggered = false;
+	}
+}
---
+++ UnityProject/Assets/PowerCapsuleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerCapsuleController : MonoBehaviour {
+
+    public BoxCollider2D PlayerBox;
+    public CircleCollider2D PlayerCircle;
+    public BoxCollider2D CapsuleBox;
+
+	// Use this for initialization
+	void Start () {
+        Physics2D.IgnoreCollision(CapsuleBox, PlayerBox);
+        Physics2D.IgnoreCollision(CapsuleBox, PlayerCircle);
+	}
+}
