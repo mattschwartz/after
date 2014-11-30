1fa261539f6e54093db337c98439826d57aef39a
mattschwartz <mattschwartz@utexas.edu>
2014-11-30 16:00:25 -0600

    Added ladder prompt for mobile in sewer level.

---
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/Interactables/Transitions/DestroyPromptTransition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Interactable;
+using After.Interactable.Transitions;
+
+namespace After.ProximityTrigger.ProximityTransitions
+{
+    public class DestroyPromptTransition : ProximityTransition 
+    {
+        public GameObject ToDestroy;
+
+        public override void Read(StateType fromState, StateType toState)
+        {
+            Destroy(ToDestroy);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/LadderPromptController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Scene.SceneManagement;
+
+public class LadderPromptController : MonoBehaviour 
+{
+    public GameObject PromptDestroyerGameObject;
+    public GameObject LadderPromptGameObject;
+
+    void Start() 
+    {
+        if (!SceneHandler.OnMobile) {
+            Destroy(PromptDestroyerGameObject);
+            Destroy(LadderPromptGameObject);
+        }
+    }
+}
