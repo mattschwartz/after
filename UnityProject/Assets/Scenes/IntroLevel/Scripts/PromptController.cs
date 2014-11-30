956e8fdc6c3c9426b1b8968b33a8053ac06a3df9
mattschwartz <mattschwartz@utexas.edu>
2014-11-30 15:40:54 -0600

    Added control prompts for mobile.

---
+++ UnityProject/Assets/Scenes/IntroLevel/Scripts/PromptController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Scene.SceneManagement;
+
+public class PromptController : MonoBehaviour 
+{
+    public GameObject MovementPromptOtherGameObject;
+    public GameObject InteractPromptOtherGameObject;
+    public GameObject InteractPromptOtherGameObject2;
+
+    public GameObject MovementPromptMobileGameObject;
+    public GameObject InteractPromptMobileGameObject;
+    public GameObject InteractPromptMobileGameObject2;
+    public GameObject JumpPromptGameMobileObject;
+
+    void Start() 
+    {
+        if (!SceneHandler.OnMobile) {
+            MobileStart();
+        } else {
+            OtherStart();
+        }
+    }
+
+    private void MobileStart()
+    {
+        Destroy(MovementPromptOtherGameObject);
+        Destroy(InteractPromptOtherGameObject);
+        Destroy(InteractPromptOtherGameObject2);
+    }
+
+    private void OtherStart()
+    {
+        Destroy(MovementPromptMobileGameObject);
+        Destroy(InteractPromptMobileGameObject);
+        Destroy(InteractPromptMobileGameObject2);
+        Destroy(JumpPromptGameMobileObject);
+    }
+}
