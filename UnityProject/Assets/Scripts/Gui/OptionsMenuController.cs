4bc240cd227b139c103c83982bdaa97fc02617b4
Matt Schwartz <schwamat@gmail.com>
2014-12-01 19:17:59 -0600

    Updated options menu.

--- UnityProject/Assets/Scripts/Gui/OptionsMenuController.cs
+++ UnityProject/Assets/Scripts/Gui/OptionsMenuController.cs
@@ -3,6 +3,7 @@ using After.Audio;
 using UnityEngine;
 using After.Scene.SceneManagement;
 using After.Journal;
+using System.Collections.Generic;
 
 namespace After.Gui
 {
@@ -10,21 +11,25 @@ namespace After.Gui
     {
         #region Members
 
-        public float hSliderValue;
-        public GUIStyle opaCustomStyle;
+        public float ClickClipVolume;
+        public GUIStyle TitleLabelStyle;
+        public GUIStyle VolumeLabelStyle;
         public GUIStyle SliderStyle;
         public GUIStyle SliderThumbStyle;
         public GUIStyle JournalStyle;
         public GUIStyle MainMenuStyle;
         public Texture MenuBackgroundTexture;
+        public AudioClip ClickClip;
 
         private bool Visible = false;
+        private float hSliderValue;
         private Rect MenuBackgroundBounds;
         private Rect SoundSliderBounds;
-        private Rect JournalIconBounds;
+        private Rect JournalBounds;
         private Rect MainMenuBounds;
         private Rect TitleBounds;
         private Rect LabelBounds;
+        private List<float> Notes;
 
         #endregion
 
@@ -32,13 +37,35 @@ namespace After.Gui
         {
             hSliderValue = AudioListener.volume * 100f;
             Resize();
+
+            Notes = new List<float>() {
+                0.05946309435905f,
+                0.12246204830885f,
+                0.1892071150019f,
+                0.2599210498937f,
+                0.3348398541685f,
+                0.4142135623711f,
+
+                -0.05946309435905f,
+                -0.12246204830885f,
+                -0.1892071150019f,
+                -0.2599210498937f,
+                -0.3348398541685f,
+                -0.4142135623711f
+            };
+        }
+
+        private void PlayRandomClick()
+        {
+            float pitch = 1 + (Notes[Random.Range(0, Notes.Count - 1)]);
+            AudioManager.PlayClipAtPoint(ClickClip, pitch, transform.position, ClickClipVolume);
         }
 
         void Update()
         {
             if (SceneHandler.GUILock != this
-                && SceneHandler.GUILock != null) { 
-                return; 
+                && SceneHandler.GUILock != null) {
+                return;
             }
 
             ProcessKeyboard();
@@ -77,34 +104,45 @@ namespace After.Gui
 
         private void Resize()
         {
-            var screenCoords = new Vector3(0.5f, 0.5f, 0);
-            var camPos = Camera.main.ViewportToScreenPoint(screenCoords);
+            float scale = ((float)Screen.width / 960f);
+            float w = 194 * scale;
+            float h = 70 * (w / 194);
+            float th = 35f * scale;
+
+            TitleLabelStyle.fontSize = (int)((float)24 * scale);
+            VolumeLabelStyle.fontSize = (int)((float)21 * scale);
+
+            SliderThumbStyle.fixedWidth = 30f * scale;
+            SliderThumbStyle.fixedHeight = th;
+
+            var screenCoords = new Vector3(0.5f, 0.36f, 0);
+            Vector3 camPos = Camera.main.ViewportToScreenPoint(screenCoords);
 
             SoundSliderBounds = new Rect {
-                x = camPos.x,
-                y = camPos.y,
-                width = 175,
-                height = 25
+                x = camPos.x - w / 2,
+                y = camPos.y - th / 2,
+                width = w,
+                height = th
             };
 
-            screenCoords = new Vector3(0, 1, 0);
-            camPos = Camera.main.ViewportToScreenPoint(screenCoords);
-
-            JournalIconBounds = new Rect {
-                x = camPos.x,
-                y = camPos.y - 149,
-                width = 146,
-                height = 149
-            };
-
-            screenCoords = new Vector3(1, 1, 0);
+            screenCoords = new Vector3(0.5f, 0.5f, 0);
             camPos = Camera.main.ViewportToScreenPoint(screenCoords);
 
             MainMenuBounds = new Rect {
-                x = camPos.x - 108,
-                y = camPos.y - 116,
-                width = 108,
-                height = 116
+                x = camPos.x - w / 2,
+                y = camPos.y - h / 2,
+                width = w,
+                height = h
+            };
+
+            screenCoords = new Vector3(0.5f, 0.65f, 0);
+            camPos = Camera.main.ViewportToScreenPoint(screenCoords);
+
+            JournalBounds = new Rect {
+                x = camPos.x - w / 2,
+                y = camPos.y - h / 2,
+                width = w,
+                height = h
             };
 
             screenCoords = new Vector3(0.5f, 0.1f, 0);
@@ -117,7 +155,7 @@ namespace After.Gui
                 height = 0
             };
 
-            screenCoords = new Vector3(0.4f, 0.5f, 0);
+            screenCoords = new Vector3(0.5f, 0.3f, 0);
             camPos = Camera.main.ViewportToScreenPoint(screenCoords);
 
             LabelBounds = new Rect {
@@ -153,24 +191,37 @@ namespace After.Gui
         {
             hSliderValue = GUI.HorizontalSlider(SoundSliderBounds,
                 hSliderValue, 0, 100, SliderStyle, SliderThumbStyle);
-            AudioListener.volume = hSliderValue / 100f;   
+            AudioListener.volume = hSliderValue / 100f;
         }
 
         private void RenderText()
         {
-            GUI.Label(TitleBounds, "Game Options", opaCustomStyle);
-            GUI.Label(LabelBounds, "Game Volume:", opaCustomStyle);
+            GUI.Label(TitleBounds, "Game Options", TitleLabelStyle);
+            GUI.Label(LabelBounds, "Game Volume:", VolumeLabelStyle);
+        }
+
+        private void MenuButton_Click()
+        {
+            PlayRandomClick();
+            Hide();
+            Application.LoadLevel("TitleScreenLevel");
+        }
+
+        private void JournalButton_Click()
+        {
+            PlayRandomClick();
+            Hide();
+            JournalController.Instance.Show();
         }
 
         private void RenderButtons()
         {
             if (GUI.Button(MainMenuBounds, GUIContent.none, MainMenuStyle)) {
-                Debug.Log("Returning to main menu.");
+                MenuButton_Click();
             }
 
-            if (GUI.Button(JournalIconBounds, GUIContent.none, JournalStyle)) {
-                Hide();
-                JournalController.Instance.Show();
+            if (GUI.Button(JournalBounds, GUIContent.none, JournalStyle)) {
+                JournalButton_Click();
             }
         }
     }
