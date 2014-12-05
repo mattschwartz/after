f11356dc2eca0fbf7e5ec39507f261e6ff7b77e0
Matt Schwartz <schwamat@gmail.com>
2014-12-04 22:53:11 -0600

    Player no longer collides with boulder in sewer level

---
+++ UnityProject/Assets/Scenes/SewerLevel/Scripts/DisableColliderController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class DisableColliderController : MonoBehaviour 
+{
+    public SpriteRenderer SRenderer;
+    public GameObject WatchFor;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == WatchFor) {
+            WatchFor.layer = LayerMask.NameToLayer("PlayerIgnores");
+            SRenderer.sortingLayerName = "Default";
+        }
+    }
+}
