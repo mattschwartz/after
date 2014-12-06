50c363c1ef7794b180080060920b8d8468f1a113
Matt Schwartz <schwamat@gmail.com>
2014-12-06 11:23:48 -0600

    Added restart functionality from credit level

---
+++ UnityProject/Assets/Scenes/CreditLevel/Scripts/CreditLevelController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using After.Scene.SceneManagement;
+
+public class CreditLevelController : MonoBehaviour
+{
+    public GameObject ReturnPromptOther;
+    public GameObject ReturnPromptMobile;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (SceneHandler.OnMobile) {
+            Destroy(ReturnPromptOther);
+        } else {
+            Destroy(ReturnPromptMobile);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.LoadLevel("TitleScreenLevel");
+        }
+    }
+}
