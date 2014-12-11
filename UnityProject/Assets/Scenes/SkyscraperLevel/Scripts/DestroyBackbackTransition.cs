2e751b6865af08413a15130e618eb12dea181b67
Matt Schwartz <schwamat@gmail.com>
2014-12-10 20:36:50 -0600

    Added audio transitions in Skyscraper level

---
+++ UnityProject/Assets/Scenes/SkyscraperLevel/Scripts/DestroyBackbackTransition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using After.Interactable.Transitions;
+using After.Interactable;
+
+public class DestroyBackbackTransition : Transition
+{
+    public List<GameObject> ToDestroy;
+
+    public override void Read(StateType fromState, StateType toState)
+    {
+        if (ToDestroy == null) { return; }
+
+        ToDestroy.ForEach(t => Destroy(t));
+    }
+}
